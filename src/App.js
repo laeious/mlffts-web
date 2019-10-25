6f2455f6bf5c561c16df3d1fa62242cd059918ee
laeious <44135105+laeious@users.noreply.github.com>
2019-10-25 07:14:38 +0700

    fix & add router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
 import './bulma.css';
 import Form from './components/Form'
+import { Switch, Route, Link, BrowserRouter as Router, } from 'react-router-dom'
 
 function App() {
   return (
-
-    <div className="section">
-      <div className="columns is-centered">
-        <div className="column is-half ">
-          <div className="container">
-            <Form />
+    <Router>
+      <div className="section">
+        <div className="columns is-centered">
+          <div className="column is-half is-centered">
+            <div className="container">
+              <Route path="/register">
+                <Form />
+              </Route>
+              <Route path="/" exact>
+                <Link to="/register" ><button className="button is-primary" style={{margin:" 0 auto"}}>Register</button></Link>
+              </Route>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Router>
 
 
   );
